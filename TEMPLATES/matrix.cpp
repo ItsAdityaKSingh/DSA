0c11341169050479c9a5c5d5b1741dfbd53884a3
lelouch-of-the-code <1aniruddhpatil.01@gmail.com>
2022-01-01 03:07:17 +0530

    fixed RRE

--- TEMPLATES/matrix.cpp
+++ TEMPLATES/matrix.cpp
@@ -6,31 +6,26 @@ struct matrix {
     int rows=0;
     int cols=0;
     vector<vector<T>> v;
-
     // constructor
     matrix(int n,int m){
         rows=n;
         cols=m;
         v=vector<vector<T>>(rows,vector<T>(cols));
     };
-
     // build matrix from stdin
     void build(){
         for(int i=0;i<rows;i++)
             for(int j=0;j<cols;j++)
                 cin>>v[i][j];
     };
-
     // access the element at (i,j)
     T get(int i,int j){
         return v[i][j];
     };
-
     // shape
     pair<int,int> shape(){
         return {rows,cols};
     };
-
     // print
     void print(){
         for(int i=0;i<rows;i++){
@@ -61,7 +56,6 @@ matrix<T> multiply(matrix<T> a,matrix<T> b){
     return res;
 };
 
-// returns row reduced echelon form of given matrix
 template<typename T>
 matrix<T> RRE(matrix<T> a){
     matrix<T> res=a;
@@ -74,7 +68,6 @@ matrix<T> RRE(matrix<T> a){
         }
         return x[0]<y[0];
     });
-
     for(int i=0;i<res.rows;i++){
         T LNZ=0;
         int idx=-1;
@@ -85,14 +78,13 @@ matrix<T> RRE(matrix<T> a){
                 break;
             }
         }
-
         if(idx==-1)
             continue;
-
         for(int j=0;j<res.cols;j++)
             res.v[i][j]/=LNZ;
-        
-        for(int j=i+1;j<res.rows;j++) {
+        for(int j=0;j<res.rows;j++) {
+            if(j==i)
+                continue;
             T fac=res.get(j,idx);
             if(fac==0)
                 continue;
@@ -105,15 +97,12 @@ matrix<T> RRE(matrix<T> a){
 
 int32_t main() {
     int n1,m1,n2,m2;
-
     cin>>n1>>m1;
     matrix<double> X1(n1,m1);
     X1.build();
-
     cin>>n2>>m2;
     matrix<double> X2(n2,m2);
     X2.build();
-
     matrix<double> X1_ech = RRE(X1);
     matrix<double> X1X2 = multiply(X1,X2);
     X1_ech.print();
