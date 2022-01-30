a15bbdb77b320c871a71d898b31037c15e0e5146
lelouch-of-the-code <1aniruddhpatil.01@gmail.com>
2022-01-31 01:52:05 +0530

    updated segtree

--- TEMPLATES/segtree.cpp
+++ TEMPLATES/segtree.cpp
@@ -1,23 +1,88 @@
+/*                                █                                  
+                                 ███                                 
+                                ██  █                                
+                               ██ ████                               
+                              ██  █   █                              
+                             ██ ████ ███                             
+                            ██  █    █  █                            
+                           ██ ████  ██████                           
+                          ██  █   ███     █                          
+                         ██ ████ ██  █   ███                         
+                        ██  █    █ ████ ██  █                        
+                       ██ ████  ██ █    █ ████                       
+                      ██  █   ███  ██  ██ █   █                      
+                     ██ ████ ██  ███ ███  ██ ███                     
+                    ██  █    █ ███   █  ███  █  █                    
+                   ██ ████  ██ █  █ █████  ███████                   
+                  ██  █   ███  ████ █    ███      █                  
+                 ██ ████ ██  ███    ██  ██  █    ███                 
+                ██  █    █ ███  █  ██ ███ ████  ██  █                
+               ██ ████  ██ █  ██████  █   █   ███ ████               
+              ██  █   ███  ████     ████ ███ ██   █   █              
+             ██ ████ ██  ███   █   ██    █   █ █ ███ ███             
+            ██  █    █ ███  █ ███ ██ █  ███ ██ █ █   █  █            
+           ██ ████  ██ █  ███ █   █  ████   █  █ ██ ██████           
+          ██  █   ███  ████   ██ █████   █ █████ █  █     █          
+         ██ ████ ██  ███   █ ██  █    █ ██ █     █████   ███         
+        ██  █    █ ███  █ ██ █ ████  ██ █  ██   ██    █ ██  █        
+       ██ ████  ██ █  ███ █  █ █   ███  ████ █ ██ █  ██ █ ████       
+      ██  █   ███  ████   ████ ██ ██  ███    █ █  ████  █ █   █      
+     ██ ████ ██  ███   █ ██    █  █ ███  █  ██ ████   ███ ██ ███     
+    ██  █    █ ███  █ ██ █ █  █████ █  ██████  █   █ ██   █  █  █    
+   ██ ████  ██ █  ███ █  █ ████     ████     ████ ██ █ █ █████████   
+  ██  █   ███  ████   ████ █   █   ██   █   ██    █  █ █ █        █  
+ ██ ████ ██  ███   █ ██    ██ ███ ██ █ ███ ██ █  █████ █ ██      ███ 
+██  █    █ ███  █ ██ █ █  ██  █   █  █ █   █  ████     █ █ █    ██  █
+                can a pattern figure out the rule?
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define db long double
+#define pb push_back
+#define pf push_front
+#define ub upper_bound
+#define lb lower_bound
+#define F first
+#define S second
+#define vll vector<ll>
+#define fin(i,n1,n2) for(int i=n1;i<n2;i++)
+#define nif(i,n1,n2) for(int i=n1;i>n2;i--)
+#define all(x) x.begin(),x.end()
+#define m69 998244353
+#define m420 1000000007
+void spit(auto x){for(auto e:x)cout<<e<<" ";cout<<endl;}
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)) && 1)
 
 template <typename T>
 struct segtree{
     int n;
+    // base case
+    // 0 for addition, gcd, xor
+    // 1 for multiplication
+    // -inf for max
+    // inf for min
+    // etc
     int BASE_CASE=0;
     vector<T> tree;
- 
-    T f(T a,T b){
-        return a+b;
-    }
- 
-    void build(int len,T* ar){
-        n=len;
+
+    // constructor
+    segtree(vector<T> &ar){
+        n=ar.size();
         tree=vector<T>(4*n+1,0);
         init(ar,1,0,n-1);
     }
- 
-    T init(T ar[],int v,int tl,int tr){
+
+    // associative func
+    // change as per need to
+    // __gcd(a,b), a*b, a^b,min,max etc
+    T f(T a,T b){
+        return a+b;
+    }
+
+    T init(vector<T> &ar,int v,int tl,int tr){
         if (tl == tr)
             return tree[v] = ar[tl];
         
@@ -26,62 +91,69 @@ struct segtree{
         T b=init(ar, v*2+1, tm+1, tr);
         return tree[v] = f(a,b);
     }
- 
+
+    // black box:
+    // gives value of function f for range [l,r]
+    // l and r shud be 0 indexed
+    // eg st.ask(l,r)
     T ask(int l,int r){
-        return query(1,0,n-1,l,r);
+        return _ask(1,0,n-1,l,r);
     }
- 
-    T query(int v,int tl,int tr,int l,int r){
+    // black box:
+    // change the value at idx to val
+    // idx has 0 indexing
+    void update(int idx, T val){
+        _update(1,0,n-1,idx,val);
+    }
+
+    T _ask(int v,int tl,int tr,int l,int r){
         if(l>r)
             return BASE_CASE;
         if(l==tl&&r==tr)
             return tree[v];
         int tm=(tl+tr)/2;
-        T a=query(v*2, tl, tm, l, min(r, tm));
-        T b=query(v*2+1, tm+1, tr, max(l, tm+1), r);
+        T a=_ask(v*2, tl, tm, l, min(r, tm));
+        T b=_ask(v*2+1, tm+1, tr, max(l, tm+1), r);
         return f(a,b);
     }
- 
-    void update(int idx, T val){
-        update(1,0,n-1,idx,val);
-    }
- 
-    void update(int v,int tl,int tr,int idx,T val){
+    void _update(int v,int tl,int tr,int idx,T val){
         if(tl==tr){
             tree[v]=val;
             return;
         }
         int tm=(tl+tr)/2;
         if(idx<=tm)
-            update(v*2,tl,tm,idx,val);
+            _update(v*2,tl,tm,idx,val);
         else
-            update(v*2+1,tm+1,tr,idx,val);
+            _update(v*2+1,tm+1,tr,idx,val);
         tree[v]=f(tree[v*2],tree[v*2+1]);
     }
 };
 
-int main() {
-    int n;
+int32_t main() {
+    ll n;
     cin>>n;
-    int ar[n];
-    for(int i=0;i<n;i++){
-        cin>>ar[i];
-    }
-    segtree<int> st;
-    st.build(n,ar);
-    int q;
+    vector<ll> ar(n);
+    fin(i,0,n)
+    	cin>>ar[i];
+    // instanciating segtree from ar
+    segtree<ll> st(ar);
+    ll q;
     cin>>q;
-    while(q--){
-        string s;
-        cin>>s;
-        if(s=="update"){
-            int idx,newv;
-            cin>>idx>>newv;
-            st.update(idx-1,newv);
-        }else{
-            int l,r;
-            cin>>l>>r;
-            cout<<st.ask(l-1,r-1)<<endl;
-        }
+    while(q--) {
+    	ll type;
+    	cin>>type;
+    	if(type) {
+    		ll l,r;
+    		cin>>l>>r;
+    		// query for value of associative
+    		// func b/w l and r
+    		cout<<st.ask(l,r)<<endl;
+    	} else {
+    		ll idx,val;
+    		cin>>idx>>val;
+    		// change value at idx to val
+    		st.update(idx,val);
+    	}
     }
 }
